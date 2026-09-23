5cfc27a557aeecdb41246098f5a12508832d5c45
axelantonia <axel.antonia2@gmail.com>
2026-09-23 19:11:44 +0700

    Feat: add button to switch view

--- app/src/main/java/com/example/explisitintent/MainActivity2.kt
+++ app/src/main/java/com/example/explisitintent/MainActivity2.kt
@@ -1,31 +1,20 @@
 package com.example.explisitintent
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        var _btnExplisit1 = findViewById<Button>(R.id.btnExplisit1)
-        _btnExplisit1.setOnClickListener {
-            val intent = Intent(
-                this@MainActivity,
-                MainActivity2::class.java
-            )
-            startActivity(intent)
         }
     }
 }
